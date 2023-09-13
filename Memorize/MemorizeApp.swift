81857e73faa41f146bc964ad9822c8acd0e3020d
Thura Khant Thein <thurakhant@gmail.com>
2023-09-13 16:46:31 +0630

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Grumman Holdings on 13/9/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
