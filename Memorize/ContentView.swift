7cea59c13b76e80b94b8b47c6ca29d1c9d883a51
Thura Khant Thein <thurakhant@gmail.com>
2023-09-14 13:27:38 +0630

    Created CardView isFacedUp

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -2,25 +2,47 @@
 //  ContentView.swift
 //  Memorize
 //
-//  Created by Grumman Holdings on 13/9/23.
+//  Created by Thura Khant Thein on 13/9/23.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, Memorize!")
-        }
+        HStack(content: {
+            CardView(isFacedUp: true)
+            CardView()
+            CardView()
+            CardView()
+        })
+        .foregroundColor(.orange)
         .padding()
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CardView: View{
+    var isFacedUp: Bool = false
+    
+    var body: some View {
+        ZStack (content : {
+            if isFacedUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2).foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("🤌").font(.largeTitle)
+            }else{
+                RoundedRectangle(cornerRadius: 12)
+            }
+            
+        })
+        
     }
 }
